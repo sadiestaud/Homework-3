2e589070bcab7ae27d402e54ca9f326bc67614cf
Sadie Staudacher <sadiestaudacher@Sadies-MacBook-Pro.local>
2017-02-05 20:00:14 -0500

    adding some of part 2

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -31,16 +31,22 @@ def parse_counted_words(x):
 ## PART 2: 200 points
 
 ## We have provided a text file computer_paths.txt. It's not incredibly long -- you can scan through it, but do NOT hard code your answers! Each line contains 1 filesystem path.
-
+f = open("computer_paths.txt", "r")
+lines = f.read()
 ## (a) Write Python code to determine how many of these paths identify FILES, not directories. Save that number in the variable file_paths_num.
-
+y = re.findall("\.\w+", lines)
+file_paths_num = len(y)
 ## (b) Write Python code to determine how many of these paths are FULL paths, not relative paths. Save that number in the variable full_paths_num.
+# y = re.findall("^\/|~", lines)
+
 
 ## (c) Write Python code to determine how many of these paths describe a Python file saved inside a folder called SI206. Save that number in the variable python_course_paths.
-
-## (d) Write Python code to determine how many of these paths describe a Microsoft file (a file that EITHER ends with .docx OR .xlsx, but nothing else counts) where the file name ends in a digit. Save that total in the variable microsoft_files_num.
-
-
+y = re.findall("Jackie\/Documents\/(SI206)", lines)
+python_course_paths = len(y)
+## (d) Write Python code to determine how many of these paths describe a Microsoft file (a file that EITHER ends with .docx OR .xlsx, 
+    # but nothing else counts) where the file name ends in a digit. Save that total in the variable microsoft_files_num.
+y = re.findall("\d\.(docx|xlsx)", lines)
+microsoft_files_num = len(y)
 
 
 
