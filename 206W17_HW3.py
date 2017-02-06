371ed04def330e8c73763ddf5601c8616462ae93
Sadie Staudacher <sadiestaudacher@Sadies-MacBook-Pro.local>
2017-02-05 19:01:32 -0500

    testing

--- 206W17_HW3.py
+++ 206W17_HW3.py
@@ -11,6 +11,7 @@ import re
 
 ## PART 1: 300 points
 
+
 ## Use regular expressions to define a function called parse_counted_words which should accept a string, find strings of the form: <count> <word>  e.g. 101 Dalmations, inside it, and should return either the LAST correctly matching number-word combo in the string as a tuple, e.g. ('13', "pineapples"), if there are any such sub-strings, or None, if there are not.
 ## The number in the <count> should be one or more digits only. The word should be made up of an optional non-alphabetic character followed by any number of alphabetic characters, upper or lower case.
 ## HINT:  \b matches the beginning or end of a word
@@ -19,14 +20,6 @@ import re
 # parse_counted_words('101 dalmations!') should return ('101', 'dalmations') ...
 
 ## Write code to define your parse_counted_words function here.
-def parse_counted_words(x):
-    y = re.findall("(\d+)\s([^A-Z]?[a-z]+)", x)
-    if len(y) > 0:
-        return y[-1]
-    else:
-        return None
-
-y = "tes"
 
 
 ## PART 2: 200 points
